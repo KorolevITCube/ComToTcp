07926ce8b1f541c88aab789860dfc47e6193e1ec
Евгений Королев <korolev.work.email@gmail.com>
2022-12-26 09:37:56 +0300

    change disconnect logic

--- src/main/java/nlmk/com/TcpController.java
+++ src/main/java/nlmk/com/TcpController.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 public class TcpController implements Runnable{
     private Socket clientDialog;
     private Orchestrator orchestrator;
-    private byte[] response = null;
+    private volatile byte[] response = null;
     private static final Logger log = LoggerFactory.getLogger(TcpController.class);
 
     public TcpController(Socket clientDialog) {
@@ -27,21 +27,21 @@ public class TcpController implements Runnable{
 
     @Override
     public void run() {
+        boolean stopFlag = false;
+        new Thread(new ResponseConsumer());
         try(DataInputStream in = new DataInputStream(clientDialog.getInputStream());
             DataOutputStream out = new DataOutputStream(clientDialog.getOutputStream());) {
             log.info("DataInputStream created");
             log.info("DataOutputStream  created");
 
-            while (!clientDialog.isClosed()) {
-                if(response != null){
-                    out.write(response,0,response.length);
-                    out.flush();
-                    response = null;
-                }
-                if(in.available() > 0){
-                    byte[] result = readRequest(in);
+            while (!clientDialog.isClosed() && !stopFlag) {
+                byte[] result = readRequest(in);
+                if(result.length > 0 && result[0] == -1){
+                    stopFlag = true;
+                    log.info("Receive -1, client was disconnect");
+                }else {
                     log.info("Receive message from client- " + Util.convertBytesToString(result));
-                    orchestrator.addRequestToQueue(new RequestWrapper(result,this));
+                    orchestrator.addRequestToQueue(new RequestWrapper(result, this));
                 }
             }
             log.info("Client disconnected");
@@ -52,19 +52,48 @@ public class TcpController implements Runnable{
         }
     }
 
+    private class ResponseConsumer implements Runnable{
+
+        boolean stopFlag = false;
+
+        public void stopThread(){
+            stopFlag = true;
+        }
+
+        @Override
+        public void run() {
+            try(DataOutputStream out = new DataOutputStream(clientDialog.getOutputStream())){
+                while(!stopFlag){
+                    if(response != null){
+                        out.write(response,0,response.length);
+                        log.info("Send response to client: "+ Util.convertBytesToString(response));
+                        out.flush();
+                        response = null;
+                    }
+                }
+            }catch (IOException e){
+                log.error("response consumer get error: "+e.getMessage());
+            }
+        }
+    }
+
     private byte[] readRequest(InputStream is) throws IOException {
         try {
             var buffer = new byte[128];
             byte[] result;
             Arrays.fill(buffer, (byte) -1);
             var counter = 0;
-            byte current = -1;
+            byte current = -2;
             do {
                 current = (byte) is.read();
                 buffer[counter] = current;
                 counter++;
-            } while (current != 0x03);
-            result = Arrays.copyOfRange(buffer, 0, counter);
+            } while (current != 0x03 || current != -1);
+            if(current == -1){
+                result = new byte[]{-1};
+            }else {
+                result = Arrays.copyOfRange(buffer, 0, counter);
+            }
             return result;
         }catch(Exception e){
             log.error("Cant read data from input stream " + e.getCause());
